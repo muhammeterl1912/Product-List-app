1bef78ca7635bdfa4ff8b6699c20825cda56250f
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-03-24 18:52:18 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Header from "./components/header/Header";
 import ProductList from "./components/products/ProductList";
-import { products, categories } from "./helper/data";
+import { categories, products } from "./helper/data";
 import "./App.scss";
+
 function App() {
- 
+  // console.log(products, categories);
   return (
-    <>
-      <Header categories ={categories} text = "Products List" />
-      <ProductList products = {products} />
-    </>
+    <div className="App">
+      <Header categories={categories} text="Products List" />
+    </div>
   );
 }
 
