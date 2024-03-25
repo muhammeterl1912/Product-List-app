d360157f9c612cd3f8fb26d1caa83a99fc54e706
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-03-25 22:58:29 +0100

    Best Practice code for Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,29 +5,20 @@ import { useState } from "react";
 
 const Header = ({ categories, text }) => {
   const [show, showData] = useState(products);
-
-  const filteredData1 = products.filter(
-    (item) => item.category === categories[1]
-  );
-  const filteredData2 = products.filter(
-    (item) => item.category === categories[2]
-  );
-  const filteredData3 = products.filter(
-    (item) => item.category === categories[3]
-  );
-  const filteredData4 = products.filter(
-    (item) => item.category === categories[4]
-  );
+  const handleButton = (e) => {
+    const filteredData1 = products.filter(
+      (item) => item.category === categories[e]
+    );
+    showData(e === 0 ? products : filteredData1);
+  };
 
   return (
     <div className={HeaderStyles.header}>
       <h1>{text} </h1>
       <div className={HeaderStyles.btns}>
-        <button onClick={() => showData(products)}>{categories[0]}</button>
-        <button onClick={() => showData(filteredData1)}>{categories[1]}</button>
-        <button onClick={() => showData(filteredData2)}>{categories[2]}</button>
-        <button onClick={() => showData(filteredData3)}>{categories[3]}</button>
-        <button onClick={() => showData(filteredData4)}>{categories[4]}</button>
+        {categories.map((item, i) => (
+          <button onClick={() => handleButton(i)}>{item}</button>
+        ))}
       </div>
       <ProductList products={show} />
     </div>
