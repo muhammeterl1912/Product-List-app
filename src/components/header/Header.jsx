0a1fae90a68f4cc254275d887ab4b347abc462c3
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-04-02 22:55:06 +0200

    added new feature which is SearchBox and styled with scss.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,9 +2,15 @@ import HeaderStyles from "./Header.module.scss";
 import { products, categories } from "../../helper/data";
 import ProductList from "../products/ProductList";
 import { useState } from "react";
+import { computeHeadingLevel } from "@testing-library/react";
 
 const Header = ({ categories, text }) => {
   const [show, showData] = useState(products);
+  const [inputVal,setInputVal] = useState("")
+
+  const handleChange = (e)=> {
+setInputVal(e.target.value)
+  }
   const handleButton = (e) => {
     const filteredData1 = products.filter(
       (item) => item.category === categories[e]
@@ -15,12 +21,15 @@ const Header = ({ categories, text }) => {
   return (
     <div className={HeaderStyles.header}>
       <h1>{text} </h1>
+      
       <div className={HeaderStyles.btns}>
         {categories.map((item, i) => (
           <button onClick={() => handleButton(i)}>{item}</button>
         ))}
       </div>
-      <ProductList products={show} />
+      <input type="text" placeholder="Search Product name.." className={HeaderStyles.input}
+      onChange={handleChange}/>
+      <ProductList products={show} inputVal={inputVal}/>
     </div>
   );
 };
--- src/components/products/ProductList.jsx
+++ src/components/products/ProductList.jsx
@@ -1,12 +1,23 @@
 import ProductCard from "./ProductCard";
 import "./Products.scss";
-const ProductList = ({ products }) => {
-
+const ProductList = ({ products,inputVal }) => {
+console.log(inputVal)
+console.log(products)
+const filteredData = products.filter((item) => item.title.toLowerCase().includes(inputVal.trim().toLowerCase()) )
   return (
     <div className="products-list">
-      {products.map((product) => (
+  
+      {
+      filteredData.map((product) => (
         <ProductCard key={product.id} {...product} />
       ))}
+      { 
+        filteredData.length < 1 && (
+          <div>
+            <h1> Product not found...</h1>
+          </div>
+        ) 
+      }
     </div>
   );
 };
